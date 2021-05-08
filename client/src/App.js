3fb40ffc13ce4d62eeebcd124a378de244842e5e
Gabriel Petre <gabriel.petre1904@stud.acs.upb.ro>
2021-05-08 16:28:35 +0300

    ui static

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import AnnotateComponent from "./annotate/AnnotateComponent";
 function App() {
   return (
     <div className="App">
      <AnnotateComponent>
-
      </AnnotateComponent>
     </div>
   );
